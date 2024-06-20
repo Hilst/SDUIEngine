a331a279467fd12ad0ab412922b6174c68cfad84
Hilst <hilstfelipe@gmail.com>
2024-06-20 10:14:45 -0300

    sample: android

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,34 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
-    kotlin("multiplatform") version "1.9.10"
+    kotlin("multiplatform") version "2.0.0"
+    id("com.android.library") version "8.0.0"
 }
 
 group = "hilst.sdui.engine"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
+    mavenLocal()
+}
+
+android {
+    namespace = group.toString()
+    compileSdkVersion = "android-31"
+    defaultConfig {
+        minSdk = 30
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_17)
+        targetCompatibility(JavaVersion.VERSION_17)
+    }
 }
 
 kotlin {
+    // IOS TARGET
     val xcf = XCFramework()
     val iosTarget = listOf(iosX64(), iosArm64(), iosSimulatorArm64())
 
@@ -25,6 +42,12 @@ kotlin {
         }
     }
 
+    // ANDROID TARGET
+    androidTarget() {
+        publishLibraryVariants("release", "debug")
+    }
+
+    // NODEJS TARGET
     js(IR) {
         moduleName = "kmm-sdui"
         nodejs()
@@ -33,25 +56,16 @@ kotlin {
     }
 
     sourceSets {
-
-        all {
-            languageSettings.apply {
-                optIn("kotlin.js.ExperimentalJsExport")
-            }
-        }
-
         val commonMain by getting
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-
         val jsMain by getting {
-            dependsOn(commonMain)
-        }
-        val jsTest by getting {
-            dependsOn(commonTest)
+            languageSettings.apply {
+                optIn("kotlin.js.ExperimentalJsExport")
+            }
         }
     }
 }
